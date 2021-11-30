2c91e16da72c7a7e8431ba8a56d4520b8e25bdf4
Rubycarolina <rubycarolinadelacruzbasora11-gmail.com>
2021-11-30 09:26:51 -0400

    media_armonica

---
+++ media_armonica.js
@@ -0,0 +1,15 @@
+// Problema 4 La armonica
+// a y b
+// 1/a y 1/b las inversas
+// ((1/a)+(1/b))/2 el promedio
+// 1/promedio al media armonica
+
+
+const mediaArmonica = (a, b) => {
+
+    const aI = 1 / a;
+    const bI = 1 / b;
+    const prom = (aI + bI) / 2;
+
+    return 1 / prom;
+}
