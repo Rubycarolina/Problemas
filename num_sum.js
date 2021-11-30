1d92a89ac0f61f092cd577448488014976c0963a
Rubycarolina <rubycarolinadelacruzbasora11-gmail.com>
2021-11-30 09:05:59 -0400

    num_sum

---
+++ num_sum.js
@@ -0,0 +1,38 @@
+//Problema 1 y 2
+
+// Start
+
+const num = 2;
+const nom = "Jose";
+
+
+// Ciclo
+for(let i=0; i < num; ++i){
+    // Imprimir nom
+    console.log(nom);
+}
+
+
+// End
+
+
+
+
+
+// Leer numero 
+let num = 9;
+let sum = 0;
+
+// not !
+while( !(num == 0)   ){
+    sum += num;
+    --num;
+}
+
+console.log(sum);
+
+
+// Gauss
+num = 9;
+let sum_gauss = num * (num + 1)/2
+console.log(sum_gauss);
