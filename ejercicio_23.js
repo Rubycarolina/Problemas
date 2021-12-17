faca8c9393b70e6f3a9ee7c259513b06a1f719d4
Rubycarolina <rubycarolinadelacruzbasora11-gmail.com>
2021-12-16 20:49:42 -0400

    ejercicio_23

---
+++ ejercicio_23.js
@@ -0,0 +1,19 @@
+/**Diseñar el algoritmo que nos permite calcular los n primeros términos de la serie Fibonacci
+ *  (donde n es ingresado por el usuario).
+ * 
+ */
+
+ var PrimerosTerminos = parseInt(prompt ("Introduce la cantidad n que quieres calcular."));
+
+ const fibonacci = (n) => {
+ 
+     // Base case
+     if (n < 2) {
+         return n;
+     }
+ 
+     // Recursive case
+     return fibonacci(n - 1) + fibonacci(n - 2);
+ }
+ 
+ fibonacci(12);
